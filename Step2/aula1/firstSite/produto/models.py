c98e87159e7d733e61fe4fffcab19a224ce4e173
lordefps <gabrielbotaoap@gmail.com>
2022-01-30 18:21:14 -0300

    startapp produto

--- Step2/aula1/firstSite/produto/models.py
+++ Step2/aula1/firstSite/produto/models.py
@@ -1,3 +1,35 @@
+from pyexpat import model
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True, null=False)
+
+    class Meta:
+        db_table = 'categoria'
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50, blank=False, null=False)
+    valor = models.DecimalField(max_digits=5,decimal_places=2, null=False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        db_table = 'produto'
+
+    def __str__(self):
+        return self.nome
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=50, blank=False, null=False)
+    valor = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        db_table = 'servico'
+
+    def __str__(self):
+        return self.nome
