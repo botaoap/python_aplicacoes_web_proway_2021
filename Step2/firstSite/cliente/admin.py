90dc7d6f44f6697ed3a13befc080cf867db6a708
lordefps <gabrielbotaoap@gmail.com>
2022-02-06 19:02:14 -0300

    aula 2 change view on admin

--- Step2/firstSite/cliente/admin.py
+++ Step2/firstSite/cliente/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 from .models import TpPessoa, CPFCNPJ, UF, Cidade, Cliente
 
 # Register your models here.
+class VisualizaCLiente(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'cpfcnpj','estadoCivil']
+    list_display_links = ['id','nome']
+    list_per_page = 5
+    # search_fields = ['nome','email']
+    # fields = ['nome', 'email', 'estadoCivil']
+    fieldsets = (
+        ('Básico', {'fields':('nome','cpfcnpj','genero','data_nasc','estadoCivil')}),
+        ('Opicional',{'fields':('filho','carro')}),
+        ('Financeiro',{'fields':('profissao','renda')})
+    )
+    
 admin.site.register(TpPessoa)
 admin.site.register(CPFCNPJ),
 admin.site.register(UF)
 admin.site.register(Cidade)
-admin.site.register(Cliente)
+admin.site.register(Cliente, VisualizaCLiente)
